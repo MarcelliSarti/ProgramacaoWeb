00641b930aace011c7aa6829aaef78512e7301cb
Harlock221B <gabrielferreira221b@gmail.com>
2022-11-07 15:29:13 -0300

    Inserindo metodos para ativar o modal no alterar

--- js/alterar.js
+++ js/alterar.js
@@ -1,3 +1,60 @@
-function temCerteza() {
-  var algumaCoisa = 0;
+function cancel(){
+  let modal = document.getElementById("modal");
+  modal.style.display = 'none';
+}
+
+function temCerteza(){
+  let name = document.getElementById("name");
+  let tel = document.getElementById("number");
+  let email = document.getElementById("email")
+  let senha = document.getElementById("txtSenha");
+
+  if(name === ""){
+    focus(name);
+    alert("Nome Invalido");
+  }
+  else if(tel === ""){
+    focus(tel);
+    alert("Numero Invalido");
+  }
+  else if(email === ""){
+    FocusEvent(email);
+    alert("Email Invalido");
+  }
+  else if(senha === ""){
+    FocusEvent(senha);
+    alert("Senha invalida")
+  }
+  else{
+    let modal = document.getElementById("modal");
+    modal.style.display = 'block';
+  }
+  
+}
+
+function salvar(){
+  let name = document.getElementById("name");
+  let tel = document.getElementById("number");
+  let email = document.getElementById("email")
+  let senha = document.getElementById("txtSenha");
+
+  if(name === ""){
+    focus(name);
+    alert("Nome Invalido");
+  }
+  else if(tel === ""){
+    focus(tel);
+    alert("Numero Invalido");
+  }
+  else if(email === ""){
+    FocusEvent(email);
+    alert("Email Invalido");
+  }
+  else if(senha === ""){
+    FocusEvent(senha);
+    alert("Senha invalida")
+  }
+  else{
+    alert("seus dados foram alterados")
+  }
 }
